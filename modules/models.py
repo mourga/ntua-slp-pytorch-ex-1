39b831396716c7138aa832ee83fdc8f26d09c672
Katerina Margatina <katerina.margatina@gmail.com>
2018-04-23 19:29:29 +0300

    Model Definition, Optimizer, Loss Function complete.

--- modules/models.py
+++ modules/models.py
@@ -1,8 +1,10 @@
-from torch import nn
+import torch
+from torch import nn, autograd
+import torch.nn.functional as F
 
 
-class BaselineModel(nn.Module):
-    def __init__(self, **kwargs):
+class BaselineLSTMModel(nn.Module):
+    def __init__(self, embedding_dim, hidden_dim, vocab_size, output_size):
         """
         Define the layers and initialize them.
 
@@ -14,10 +16,29 @@ class BaselineModel(nn.Module):
 
         Args:
         """
-        super(BaselineModel, self).__init__()
-        pass
+        super(BaselineLSTMModel, self).__init__()
+        self.hidden_dim = hidden_dim
+        self.word_embeddings = nn.Embedding(vocab_size, embedding_dim)
 
-    def forward(self, x, lengths):
+        # The LSTM takes word embeddings as inputs, and outputs hidden states
+        # with dimensionality hidden_dim.
+        self.lstm = nn.LSTM(embedding_dim, hidden_dim)
+
+        # The linear layer that maps from hidden state space to tag space
+        self.hidden2output = nn.Linear(hidden_dim, output_size)
+        self.hidden = self.init_hidden()
+
+    def init_hidden(self):
+        # Before we've done anything, we dont have any hidden state.
+        # The axes semantics are (num_layers, minibatch_size, hidden_dim)
+        # the first is the hidden h
+        # the second is the cell c
+        return (autograd.Variable(torch.zeros(1, 1, self.hidden_dim)),
+                autograd.Variable(torch.zeros(1, 1, self.hidden_dim)))
+
+
+
+    def forward(self, sentence):
         """
         This is the heart of the model.
         This function, defines how the data passes through the network.
@@ -25,4 +46,9 @@ class BaselineModel(nn.Module):
         Returns: the logits for each class
 
         """
-        pass
+        embeds = self.word_embeddings(sentence)
+        x = embeds.view(len(sentence), 1, -1)
+        lstm_out, self.hidden = self.lstm(x, self.hidden)
+        y = self.hidden2output(lstm_out.view(len(sentence), -1))
+        output_scores = F.log_softmax(y, dim=1)
+        return output_scores
--- train.py
+++ train.py
@@ -1,8 +1,11 @@
 # download from http://nlp.stanford.edu/data/glove.twitter.27B.zip
 # WORD_VECTORS = "../embeddings/glove.twitter.27B.50d.txt"
+import torch
+from torch.autograd import Variable
 from torch.utils.data import DataLoader
-
+from torch import nn, optim
 from modules.dataloaders import SentenceDataset
+from modules.models import BaselineLSTMModel
 from utils.load_embeddings import load_word_vectors
 from utils.load_data import *
 import numpy as np
@@ -14,6 +17,7 @@ from utils.utilities import label_mapping
 
 EMBEDDINGS = "embeddings/glove.twitter.27B.50d.txt"
 EMB_DIM = 50
+HID_DIM = 100
 BATCH_SIZE = 128
 EPOCHS = 50
 
@@ -59,6 +63,11 @@ loader_test = DataLoader(test_set, batch_size=BATCH_SIZE, shuffle=True)
 
 # define a simple model, loss function and optimizer
 
+model = BaselineLSTMModel(embedding_dim=EMB_DIM, hidden_dim=HID_DIM,
+                       vocab_size=len(word2idx), output_size=len(lab2idx))
+loss_function = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=1e-3)
+# optimizer = torch.optim.SGD(model.parameters(), lr = 1e-2)
 
 #############################################################################
 # Training Pipeline
@@ -66,3 +75,96 @@ loader_test = DataLoader(test_set, batch_size=BATCH_SIZE, shuffle=True)
 
 # loop the dataset with the dataloader that you defined and train the model
 # for each batch return by the dataloader
+
+train_loss_ = []
+test_loss_ = []
+train_acc_ = []
+test_acc_ = []
+### training procedure
+for epoch in range(EPOCHS):
+
+    ## training epoch
+    total_acc = 0.0
+    total_loss = 0.0
+    total = 0.0
+    for iter, traindata in enumerate(loader_train):
+        train_inputs, train_labels = traindata
+        train_labels = torch.squeeze(train_labels)
+
+        train_inputs, train_labels = Variable(train_inputs.cuda()), train_labels.cuda()
+
+        model.zero_grad()
+        model.batch_size = len(train_labels)
+        model.hidden = model.init_hidden()
+        output = model(train_inputs.t())
+
+        loss = loss_function(output, Variable(train_labels))
+        loss.backward()
+        optimizer.step()
+
+        # calc training acc
+        _, predicted = torch.max(output.data, 1)
+        total_acc += (predicted == train_labels).sum()
+        total += len(train_labels)
+        total_loss += loss.data[0]
+
+    train_loss_.append(total_loss / total)
+    train_acc_.append(total_acc / total)
+    print("Epoch: ", iter, " train loss: ", total_loss / total, " accuracy ", total_acc / total)
+
+#     ## testing epoch
+#     total_acc = 0.0
+#     total_loss = 0.0
+#     total = 0.0
+#     for iter, testdata in enumerate(test_loader):
+#         test_inputs, test_labels = testdata
+#         test_labels = torch.squeeze(test_labels)
+#
+#         if use_gpu:
+#             test_inputs, test_labels = Variable(test_inputs.cuda()), test_labels.cuda()
+#         else:
+#             test_inputs = Variable(test_inputs)
+#
+#         model.batch_size = len(test_labels)
+#         model.hidden = model.init_hidden()
+#         output = model(test_inputs.t())
+#
+#         loss = loss_function(output, Variable(test_labels))
+#
+#         # calc testing acc
+#         _, predicted = torch.max(output.data, 1)
+#         total_acc += (predicted == test_labels).sum()
+#         total += len(test_labels)
+#         total_loss += loss.data[0]
+#     test_loss_.append(total_loss / total)
+#     test_acc_.append(total_acc / total)
+#
+#     print('[Epoch: %3d/%3d] Training Loss: %.3f, Testing Loss: %.3f, Training Acc: %.3f, Testing Acc: %.3f'
+#           % (epoch, epochs, train_loss_[epoch], test_loss_[epoch], train_acc_[epoch], test_acc_[epoch]))
+#
+# param = {}
+# param['lr'] = learning_rate
+# param['batch size'] = batch_size
+# param['embedding dim'] = embedding_dim
+# param['hidden dim'] = hidden_dim
+# param['sentence len'] = sentence_len
+
+# result = {}
+# result['train loss'] = train_loss_
+# result['test loss'] = test_loss_
+# result['train acc'] = train_acc_
+# result['test acc'] = test_acc_
+# result['param'] = param
+#
+# if use_plot:
+#     import PlotFigure as PF
+#
+#     PF.PlotFigure(result, use_save)
+# if use_save:
+#     filename = 'log/LSTM_classifier_' + datetime.now().strftime("%d-%h-%m-%s") + '.pkl'
+#     result['filename'] = filename
+#
+#     fp = open(filename, 'wb')
+#     pickle.dump(result, fp)
+#     fp.close()
+#     print('File %s is saved.' % filename)
